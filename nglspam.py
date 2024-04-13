a9f1f2cc226f57f87b4a0b00176b1aec6aa2062d
Samplayswthpython <166288160+Samplayswthpython@users.noreply.github.com>
2024-04-13 17:51:45 +0530

    Add files via upload

---
+++ nglspam.py
@@ -0,0 +1,37 @@
+import requests
+import threading
+
+url = "https://ngl.link/api/submit"
+
+
+username = input("Enter your username : ")
+txt = input("Enter Your text")
+
+
+
+data = {
+    'username': username,
+    'question': txt, 
+    'deviceId': '5adh65ye-b542-48g5-b76a-33765y242e59',
+    'gameSlug': '' ,
+    'referrer': '',
+    }
+
+
+def do_request():
+  while True:
+      response = requests.post(url, data=data).text
+      print("<Message sent> ", )
+    
+threads = []
+
+for i in range(50):
+   t = threading.Thread(target = do_request)
+   t.daemon = True
+   threads.append(t)
+
+for i in range(50):
+   threads[i].start()
+
+for i in range(50):
+   threads[i].join()
