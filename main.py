ce20b84f6e892826d5326c1be20108f085c911c0
TheBetterQwerty <166288160+TheBetterQwerty@users.noreply.github.com>
2025-03-27 23:53:55 +0530

    Update and rename nglspam.py to main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import signal
+import threading
+try:
+    import requests
+except ModuleNotFoundError:
+    print("[!] Requests module Not Found!")
+    exit()
+
+# global counter (race condition prone!)
+COUNTER: int = 0
+URL: str = "https://ngl.link/api/submit"
+exit_flag = False
+
+def post_requests(user: str, text: str) -> None:
+    global COUNTER
+    url: str = URL
+    
+    data: dict[str, str] = {
+        'username': user,
+        'question': text, 
+        'deviceId': '5adh65ye-b542-48g5-b76a-33765y242e59',
+        'gameSlug': '' ,
+        'referrer': '',
+    }
+
+    while not exit_flag:
+        try:
+            resp = requests.post(url, data=data)
+            COUNTER += 1
+            print(f"\r[$] Message's sent to {user}: {COUNTER}", end="")
+        except Exception as e:
+            print(f"\n[!] Error: {e}")
+
+def exit_prog(sig: int, frame) -> None:
+    global exit_flag
+    exit_flag = True
+
+def main() -> None:
+    user: str = input("[*] Enter the username: ")
+    text: str = input("[*] Enter the text: ")
+    
+    signal.signal(signal.SIGINT, exit_prog)
+
+    threads = []
+    for _ in range(20):
+        thread = threading.Thread(target=post_requests, args=(user, text))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    main()
+    print(f"\n\n[-]CTRL+c pressed killing all threads!")
